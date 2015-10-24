e35bb03d1e066911c83f388a218884ae6967d253
Alvin Francis <nivlafx@gmail.com>
2015-10-24 15:13:26 -0400

    Fix custombootimg and increase recovery size
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,17 +1,17 @@
-LZMA_BIN := $(shell which lzma)
+LZMA_BIN := $(shell which lzma > )
 
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(recovery_uncompressed_ramdisk) \
 		$(recovery_kernel)
-	@echo ----- Compressing recovery ramdisk with lzma ------
-	rm -f $(recovery_uncompressed_ramdisk).lzma
+	@echo -e ${CL_CYN}"----- Compressing recovery ramdisk with lzma ------"${CL_RST}
+	$(hide) rm -f $(recovery_uncompressed_ramdisk)
 	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
-	@echo ----- Making recovery image ------
+	$(hide) rsync -a $(recovery_uncompressed_ramdisk) $(recovery_ramdisk)
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	@echo ----- Made recovery image -------- $@
+	@echo -e ${CL_CYN}"----- Made recovery image -------- $@"${CL_RST}
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
 
